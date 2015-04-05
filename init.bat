42763d07d5c20381926574f0510269341792f684
sigelinde <sigelinde@live.jp>
2015-04-06 03:30:19 +0900

    Added 'git submodule' to init.bat

--- init.bat
+++ init.bat
@@ -1,3 +1,6 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
 mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\.gvimrc"
 mklink %HOMEPATH%"\.vimperatorrc" %HOMEPATH%"\dotfiles\.vimperatorrc"
+
+git submodule init
+git submodule update
